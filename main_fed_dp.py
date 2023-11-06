18fbaabbe93fa036bd952f0336c35b957767527f
macknight <macknight2013@gmail.com>
2023-11-06 17:49:40 +0800

    fix code error for fed_dp

--- main_fed_dp.py
+++ main_fed_dp.py
@@ -1,6 +1,8 @@
 import copy
 import numpy as np
 import torch
+import pydp as dp  # 导入 PyDP 模块
+from pydp.algorithms.laplacian import BoundedSum
 
 from models.Fed import FedAvg
 from models.Nets import MLP, Mnistcnn
@@ -11,14 +13,15 @@ from utils.dataset import get_dataset, exp_details
 from utils.options import args_parser
 
 if __name__ == '__main__':
-    # parse args
+    # 解析参数
     args = args_parser()
     args.device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() and args.gpu != -1 else 'cpu')
     print(f'args.device:', {args.device})
-    # load dataset and split data for users
+    
+    # 加载数据集并为用户分割数据
     dataset_train, dataset_test, dict_party_user, dict_sample_user = get_dataset(args)
 
-    # build model
+    # 建立模型
     if args.model == 'cnn' and args.dataset == 'MNIST':
         net_glob = Mnistcnn(args=args).to(args.device)
     elif args.model == 'mlp':
@@ -34,12 +37,12 @@ if __name__ == '__main__':
     empty_net = net_glob
     print('Model architecture:')
     print(net_glob)
-    net_glob.train()#Set the neural network model net _ glob to enter the training mode
+    net_glob.train()  # 将神经网络模型 net_glob 设置为训练模式
 
-    # copy weights
+    # 复制权重
     w_glob = net_glob.state_dict()
 
-    # training
+    # 训练
     if args.all_clients:
         print("Aggregation over all clients")
         w_locals = [w_glob for i in range(args.num_users)]
@@ -63,13 +66,23 @@ if __name__ == '__main__':
                 w_locals.append(copy.deepcopy(w))
             loss_locals.append(copy.deepcopy(loss))
 
-
         # implement the source inference attack
         SIA_attack = SIA(args=args, w_locals=w_locals, dataset=dataset_train, dict_sia_users=dict_sample_user)
         attack_acc = SIA_attack.attack(net=empty_net.to('cpu'))#args.device
         best_att_acc = max(best_att_acc, attack_acc)
 
         # update global weights
+        privacy_budget = 0.4  # 设置差分隐私隐私预算，根据需要调整
+        #DP add noise
+        for w_local in w_locals:
+            for key, value in w_local.items():
+                original_shape = value.shape
+                flat_list = value.view(-1).tolist()
+                w_list = BoundedSum(epsilon=privacy_budget, lower_bound=-3, upper_bound=3, dtype="float").quick_result(flat_list)
+                reshaped_tensor = torch.tensor(flat_list).view(original_shape)
+                w_local[key] = reshaped_tensor
+
+        #server aggregation
         w_glob = FedAvg(w_locals)
 
         # copy weight to net_glob
@@ -77,20 +90,21 @@ if __name__ == '__main__':
         acc_train, loss_train_ = test_fun(net_glob, dataset_train, args)
         # print loss
         loss_avg = sum(loss_locals) / len(loss_locals)
-        print('Round {:3d}, Average training loss {:.3f}'.format(iter, loss_avg))
+        print('Epoch Round {:3d}, Average training loss {:.3f}'.format(iter, loss_avg))
+        #end of each epoch of args.epochs
 
-
-
-    # testing
+    # 测试
     net_glob.eval()
     acc_train, loss_train_ = test_fun(net_glob, dataset_train, args)
     acc_test, loss_test = test_fun(net_glob, dataset_test, args)
-    # experiment setting
+    # 实验设置
     exp_details(args)
+
 
 
     print('Experimental result summary:')
     print("Training accuracy of the joint model: {:.2f}".format(acc_train))
     print("Testing accuracy of the joint model: {:.2f}".format(acc_test))
+    
     print('Random guess baseline of source inference : {:.2f}'.format(1.0/args.num_users*100))
     print('Highest prediction loss based source inference accuracy: {:.2f}'.format(best_att_acc))
