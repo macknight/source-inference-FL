a962fe6b9ef16f43d4a6dd2d8a0c318d7ad35a1a
macknight <macknight2013@gmail.com>
2023-12-07 15:27:54 +0800

    fix

--- main_fed_combine_he_dp.py
+++ main_fed_combine_he_dp.py
@@ -63,7 +63,7 @@ def process(args):
     total_attack_success_rate = 0
     total_loss_list = [0] * args.epochs
 
-    duplication = 2
+    duplication = 100
     for i in range(duplication):
         execution_time = 0
         acc_train = 0
